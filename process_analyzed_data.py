9927757b5e215556f98d41d8620a1573bae01aa9
Stanislav Marochok <xmarochok@stuba.sk>
2022-04-30 10:50:19 -0400

    added som new parameters to analyzing results

--- process_analyzed_data.py
+++ process_analyzed_data.py
@@ -20,10 +20,9 @@ def run_processing_data():
     result_df['sboxes_count'] = result_df['sboxes_count'].astype(int)
     result_df['sboxes_size'] = result_df['sboxes_size'].astype(int)
     
-    result_df['sboxes_count_with_max_item_4'] = result_df['sboxes_count_with_max_item_4'].astype(int)
-    result_df['sboxes_count_with_max_item_6'] = result_df['sboxes_count_with_max_item_6'].astype(int)
-    result_df['sboxes_count_with_max_item_8'] = result_df['sboxes_count_with_max_item_8'].astype(int)
-    result_df['sboxes_count_with_max_item_10'] = result_df['sboxes_count_with_max_item_10'].astype(int)
+    print(result_df)
+    for max_item in [4, 6, 8, 10]:
+        result_df[f'sboxes_count_with_max_item_{max_item}'] = result_df[f'sboxes_count_with_max_item_{max_item}'].fillna(0).astype(int)
 
     print(result_df.head())
 
@@ -43,24 +42,39 @@ def process_file(csv_filename, csv_folder):
     result['sboxes_count'] = get_sboxes_count(clean_csv_filename)
     result['sboxes_size'] = 2 ** get_sboxes_size(clean_csv_filename)
 
-    max_items = get_sboxes_count_with_max_item(df['max_item'])
-    result['sboxes_count_with_max_item_4'] = max_items.get('4')
-    result['sboxes_count_with_max_item_6'] = max_items.get('6')
-    result['sboxes_count_with_max_item_8'] = max_items.get('8')
-    result['sboxes_count_with_max_item_10'] = max_items.get('10')
+    max_items_column = df['max_item']
+    max_items = get_sboxes_count_with_max_item(max_items_column)
+    print(max_items)
+    for max_item in max_items.keys():
+        result[f'sboxes_count_with_max_item_{max_item}'] = max_items.get(f'{max_item}')
+
+        # TODO: get 2 columns - max_item and max_item_count
+        max_items_count_for_max_item = get_sboxes_count_with_max_item_count_for_max_item(max_item, max_items_column, df['max_item_count'])
+        for max_item_count in max_items_count_for_max_item.keys():
+            result[f'sboxes_count_with_max_item_count_{max_item_count}'] = max_items_count_for_max_item.get(f'{max_item_count}')
 
     return result
 
 
 def get_sboxes_count_with_max_item(max_items):
-    count = 0
-    max_item_result = { '4' : 0, '6' : 0, '8' : 0, '10' : 0, '12' : 0 }
+    max_item_result = {}
     for item in max_items:
         if max_item_result.get(str(item)) is None:
             max_item_result[str(item)] = 0
         max_item_result[str(item)] += 1
-    print(max_item_result)
     return max_item_result
+
+
+def get_sboxes_count_with_max_item_count_for_max_item(max_item, max_item_column, max_items_count_column):
+    max_item_count_result = {}
+    for max_item_count_index in range(len(max_items_count_column)):
+        max_item_count = max_items_count_column[max_item_count_index]
+        if max_item_count_result.get(str(max_item_count)) is None:
+            max_item_count_result[f'{max_item_count}'] = 0
+        
+        if int(max_item_column[max_item_count_index]) == int(max_item):
+            max_item_count_result[f'{max_item_count}'] += 1
+    return max_item_count_result
 
 
 def get_average_time_taken_for_sbox_generation(time_elapsed_column):
