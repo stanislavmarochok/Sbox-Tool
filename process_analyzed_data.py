d6f35d225c4714cb62b01ce302097590d5594d24
Stanislav Marochok <xmarochok@stuba.sk>
2022-04-30 17:43:55 -0400

    finished parsing of all the parameters for the processor of analyzed sboxes

--- process_analyzed_data.py
+++ process_analyzed_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import pandas as pd
 import numpy as np
@@ -12,8 +12,6 @@ def process_data():
 
     pd.set_option('display.max_columns', 10)
 
-    # process the files with pandas
-
     result_df = pd.DataFrame()
     for csv_filename in csv_filenames:
         new_row = process_file(csv_filename, csv_folder)
@@ -21,7 +19,7 @@ def process_data():
 
     result_df['sboxes_count'] = result_df['sboxes_count'].astype(int)
     result_df['sboxes_size'] = result_df['sboxes_size'].astype(int)
-    
+
     columns = [column_name for column_name in result_df.columns if 'sboxes_count_with' in column_name]
     for column_name in columns:
         result_df[column_name] = result_df[column_name].fillna(0).astype(int)
@@ -36,8 +34,7 @@ def process_file(csv_filename, csv_folder):
     full_path = f'{csv_folder}/{csv_filename}'
     df = pd.read_csv(full_path)
     time_elapsed_column = df['time_elapsed']
-    
-    # delete ending '.csv' from the filename
+
     clean_csv_filename = csv_filename[:-4]
 
     result = {}
@@ -52,14 +49,13 @@ def process_file(csv_filename, csv_folder):
     for max_item in max_items.keys():
         result[f'sboxes_count_with_max_item_{max_item}'] = max_items.get(f'{max_item}')
 
-        # TODO: get 2 columns - max_item and max_item_count
         max_items_count_for_max_item = get_sboxes_count_with_max_item_count_for_max_item(max_item, max_items_column, df['max_item_count'])
         for max_item_count in max_items_count_for_max_item.keys():
             result[f'sboxes_count_with_max_item_{max_item}_max_item_count_{max_item_count}'] = max_items_count_for_max_item.get(f'{max_item_count}')
 
-        zero_items_count_for_max_item = get_sboxes_count_with_zero_items_count_for_max_item(max_item, max_items_column, df['zero_items_count'])
-        for zero_items_count in zero_items_count_for_max_item.keys():
-            result[f'sboxes_count_with_max_item_{max_item}_zero_items_count_{zero_items_count}'] = zero_items_count_for_max_item.get(f'{zero_items_count}')
+    zero_items_count = get_sboxes_count_with_zero_items_count(max_item, df['zero_items_count'])
+    for zero_items_value in zero_items_count.keys():
+        result[f'sboxes_count_with_zero_items_value_{zero_items_value}'] = zero_items_count.get(f'{zero_items_value}')
 
     return result
 
@@ -73,15 +69,14 @@ def get_sboxes_count_with_max_item(max_items):
     return max_item_result
 
 
-def get_sboxes_count_with_zero_items_count_for_max_item(max_item, max_item_column, zero_items_count_column):
+def get_sboxes_count_with_zero_items_count(max_item, zero_items_count_column):
     zero_items_count_result = {}
     for zero_items_count_index in range(len(zero_items_count_column)):
         zero_items_count = zero_items_count_column[zero_items_count_index]
         if zero_items_count_result.get(str(zero_items_count)) is None:
             zero_items_count_result[f'{zero_items_count}'] = 0
-        
-        if int(max_item_column[zero_items_count_index]) == int(max_item):
-            zero_items_count_result[f'{zero_items_count}'] += 1
+
+        zero_items_count_result[f'{zero_items_count}'] += 1
     return zero_items_count_result
 
 
@@ -91,7 +86,7 @@ def get_sboxes_count_with_max_item_count_for_max_item(max_item, max_item_column,
         max_item_count = max_items_count_column[max_item_count_index]
         if max_item_count_result.get(str(max_item_count)) is None:
             max_item_count_result[f'{max_item_count}'] = 0
-        
+
         if int(max_item_column[max_item_count_index]) == int(max_item):
             max_item_count_result[f'{max_item_count}'] += 1
     return max_item_count_result
