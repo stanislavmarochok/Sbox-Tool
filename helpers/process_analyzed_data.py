ad65987a40bd4fb964f6e05827da21a9ba0aea24
Stanislav Marochok <ssssstas30@gmail.com>
2023-02-06 23:01:45 +0100

    updated script for gathering statistics

--- helpers/process_analyzed_data.py
+++ helpers/process_analyzed_data.py
@@ -10,7 +10,7 @@ from ExportHelper import ExportHelper
 
 
 def process_data():
-    csv_folder = './output/sboxes_datasets'
+    csv_folder = './for_article/sboxes_datasets'
     csv_filenames = os.listdir(csv_folder)
     csv_filenames = [filename for filename in csv_filenames if filename.endswith("csv")]
     csv_filenames = natsorted(csv_filenames)
@@ -27,7 +27,7 @@ def process_data():
     result_df = get_dataframe_with_reordered_columns(result_df)
 
     exportHelper = ExportHelper()
-    exportHelper.exportDataframeToCsvToFolder(result_df, 'output', 'sboxes_datasets_processed', 'statistics.csv')
+    exportHelper.exportDataframeToCsvToFolder(result_df, 'for_article', 'sboxes_datasets_processed', 'statistics.csv')
 
 
 def convert_dataframe_columns_to_int(df):
@@ -55,12 +55,20 @@ def get_dataframe_with_reordered_columns(df):
         if colname in cols:
             max_item_columns.append(colname)
 
-    max_item_count_columns = natsorted([colname for colname in cols if 'max_item_count' in colname])
-    zero_items_count_columns = natsorted([colname for colname in cols if 'zero_items_count' in colname])
+    # max_item_count_columns = natsorted([colname for colname in cols if 'max_item_count' in colname])
+    # zero_items_count_columns = natsorted([colname for colname in cols if 'zero_items_count' in colname])
 
     sorted_cols.extend(max_item_columns)
-    sorted_cols.extend(max_item_count_columns)
-    sorted_cols.extend(zero_items_count_columns)
+    # sorted_cols.extend(max_item_count_columns)
+    # sorted_cols.extend(zero_items_count_columns)
+
+    nonlinearity_columns = []
+    for column in cols:
+        if 'nonlinearity' in column:
+            nonlinearity_columns.append(column)
+
+    sorted_cols.extend(nonlinearity_columns)
+
 
     result_cols = []
     result_cols.extend(cols_to_insert_to_the_beginning)
@@ -88,17 +96,22 @@ def process_file(csv_filename, csv_folder):
     for max_item in max_items.keys():
         result[f'sboxes_count_with_max_item_{max_item}'] = max_items.get(f'{max_item}')
 
-    for max_item in max_items.keys():
-        max_items_count_for_max_item = get_sboxes_count_with_max_item_count_for_max_item(max_item, max_items_column,
-                                                                                         df['max_item_count'])
-        for max_item_count in max_items_count_for_max_item.keys():
-            result[
-                f'sboxes_count_with_max_item_{max_item}_max_item_count_{max_item_count}'] = max_items_count_for_max_item.get(
-                f'{max_item_count}')
+    # for max_item in max_items.keys():
+    #     max_items_count_for_max_item = get_sboxes_count_with_max_item_count_for_max_item(max_item, max_items_column,
+    #                                                                                      df['max_item_count'])
+    #     for max_item_count in max_items_count_for_max_item.keys():
+    #         result[
+    #             f'sboxes_count_with_max_item_{max_item}_max_item_count_{max_item_count}'] = max_items_count_for_max_item.get(
+    #             f'{max_item_count}')
+    #
+    # zero_items_count = get_sboxes_count_with_zero_items_count(max_item, df['zero_items_count'])
+    # for zero_items_value in zero_items_count.keys():
+    #     result[f'sboxes_count_with_zero_items_count_{zero_items_value}'] = zero_items_count.get(f'{zero_items_value}')
 
-    zero_items_count = get_sboxes_count_with_zero_items_count(max_item, df['zero_items_count'])
-    for zero_items_value in zero_items_count.keys():
-        result[f'sboxes_count_with_zero_items_count_{zero_items_value}'] = zero_items_count.get(f'{zero_items_value}')
+    nonlinearity_column = df['nonlinearity']
+    nolinearities = get_sboxes_count_with_nonlinearity(nonlinearity_column)
+    for nolinearity in nolinearities.keys():
+        result[f'sboxes_count_with_nonlinearity_{nolinearity}'] = nolinearities.get(f'{nolinearity}')
 
     return result
 
@@ -110,6 +123,15 @@ def get_sboxes_count_with_max_item(max_items):
             max_item_result[str(item)] = 0
         max_item_result[str(item)] += 1
     return max_item_result
+
+
+def get_sboxes_count_with_nonlinearity(nonlinearities):
+    nonlinearity_result = {}
+    for item in nonlinearities:
+        if nonlinearity_result.get(str(item)) is None:
+            nonlinearity_result[str(item)] = 0
+        nonlinearity_result[str(item)] += 1
+    return nonlinearity_result
 
 
 def get_sboxes_count_with_zero_items_count(max_item, zero_items_count_column):
